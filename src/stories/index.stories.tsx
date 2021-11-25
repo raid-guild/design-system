1b663c1b991fa2659a371724cbfb9ab8f4b56c5b
0xshred <0xshred@protonmail.com>
2021-11-24 22:31:47 -0800

    Simple Footer in stories

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -4,33 +4,34 @@ import { Heading, VStack, ChakraText, HStack, Image, Tooltip } from '..';
 import raidGuildLogoLeft from '../assets/images/raid--left.png';
 import raidGuildLogoRight from '../assets/images/raid--right.png';
 import season2 from '../assets/images/Season2-DAO-02b.png';
+import { FooterSimple } from '../theme/components/footers/Simple';
 
 export default {
   title: 'Raid Guild Design System',
 } as Meta;
 
 const Template: Story = () => (
-  <HStack justifyContent="center">
-    <Image src={raidGuildLogoLeft} alt='Swords logo' maxH='75vh' />
-    <VStack align="center" spacing="5vh">
-        <Heading
-          variant="headingOne"
-          textTransform="uppercase"
-          fontSize={{ base: '18px' }}
-          textAlign="center"
-        >
-            Raid Guild Design Guide
+  <>
+    <HStack justifyContent="center">
+      <Image src={raidGuildLogoLeft} alt="Swords logo" maxH="75vh" />
+
+      <VStack align="center" spacing="5vh">
+        <Heading as="h1" size="3xl" color="white" textAlign="center">
+          Raid Guild Design Guide
         </Heading>
         <ChakraText align="center" mb={5} fontFamily="texturina">
-            Use the navigation on the left to explore existing components
-            built with Raid Guild styling.
+          Use the navigation on the left to explore existing components built
+          with Raid Guild styling.
         </ChakraText>
+        {/* <BroughtToYouBuyRaidGuild /> */}
         <Tooltip label="Raid Guild Cohort 2" aria-label="Raid Guild Cohort 2">
-            <Image src={season2} alt="Raid Guild Cohort 2, FTW!" maxH="20vh" />
+          <Image src={season2} alt="Raid Guild Cohort 2, FTW!" maxH="20vh" />
         </Tooltip>
-    </VStack>
-    <Image src={raidGuildLogoRight} alt='Swords logo' maxH='75vh' />
-  </HStack>
+      </VStack>
+      <Image src={raidGuildLogoRight} alt="Swords logo" maxH="75vh" />
+    </HStack>
+    <FooterSimple />
+  </>
 );
 
 export const LandingPage = Template.bind({});
