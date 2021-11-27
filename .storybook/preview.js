d7f085db8f4fbd0763bb122c671a87ac5834a9d8
0xshred <0xshred@protonmail.com>
2021-11-27 15:02:29 -0800

    story book update, ally-addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-import { addDecorator } from '@storybook/react';
+// import { addDecorator } from '@storybook/react';
 import React from 'react';
 import { RGTheme, RGThemeProvider } from '../src';
 
@@ -13,22 +13,37 @@ const RaidGuildTheme = {
 };
 
 export const Chakra = ({ children }) => (
-  <RGThemeProvider>
-    {children}
-  </RGThemeProvider>
+  <RGThemeProvider>{children}</RGThemeProvider>
 );
 
-addDecorator((StoryFn) => (
-  <Chakra>
-    <StoryFn />
-  </Chakra>
-));
+// addDecorator((StoryFn) => (
+//   <Chakra>
+//     <StoryFn />
+//   </Chakra>
+// ));
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Chakra>
+        <Story />
+      </Chakra>
+    </>
+  ),
+];
 
 export const parameters = {
   viewport: {
     viewports: INITIAL_VIEWPORTS,
   },
   actions: { argTypesRegex: '^on[A-Z].*' },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: '#root',
+    // sets the execution mode for the addon
+    manual: false,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
