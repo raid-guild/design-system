033955c0024d86ae92f7ee9801f408d8fb63833c
Scott Herren <me@stwoh.com>
2021-12-18 12:10:13 -0600

    try without assets and correct import

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
-import 'assets/css/fonts.css';
-import 'assets/css/globals.css';
+import '../assets/css/fonts.css';
+import '../assets/css/globals.css';
 
 import Button from './components/Button';
 import Card from './components/Card/Card';
