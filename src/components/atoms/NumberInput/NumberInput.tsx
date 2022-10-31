7bf9dbae71453954ecdac3359ff2e4ea7b8172a1
Mike Starr <imhs@protonmail.com>
2022-10-31 19:17:17 -0400

    tep, min, max props

--- src/components/atoms/NumberInput/NumberInput.tsx
+++ src/components/atoms/NumberInput/NumberInput.tsx
@@ -13,6 +13,9 @@ import {
 
 type CustomNumberInputProps = {
   label?: string;
+  step?: number;
+  min?: number;
+  max?: number;
   name: string;
   variant: string;
   localForm: UseFormReturn;
@@ -26,6 +29,9 @@ type NumberInputProps = ChakraInputProps & CustomNumberInputProps;
  */
 const NumberInput: React.FC<NumberInputProps> = ({
   label,
+  step,
+  min,
+  max,
   name,
   variant,
   localForm,
@@ -36,7 +42,7 @@ const NumberInput: React.FC<NumberInputProps> = ({
   return (
     <FormControl>
       {label && <FormLabel>{label}</FormLabel>}
-      <ChakraNumberInput variant={variant}>
+      <ChakraNumberInput step={step} min={min} max={max} variant={variant}>
         <NumberInputField {...register(name, customValidations)} />
         <NumberInputStepper>
           <NumberIncrementStepper />
