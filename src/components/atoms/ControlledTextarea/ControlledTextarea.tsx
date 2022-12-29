1775624cc971536a48e13f5fbffb8d6b4215ad46
jonathanprozzi <jonathanprozzi@gmail.com>
2022-12-29 14:27:22 -0500

    restructured the props on the ControlledTextarea

--- src/components/atoms/ControlledTextarea/ControlledTextarea.tsx
+++ src/components/atoms/ControlledTextarea/ControlledTextarea.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { UseFormReturn } from 'react-hook-form';
-import {
-  ChakraTextarea,
-  ChakraTextareaProps,
-  FormControl,
-  FormLabel,
-} from '../../chakra';
+import { ChakraTextarea, ChakraTextareaProps, FormLabel } from '../../chakra';
 
 type CustomControlledTextareaProps = {
-  label: string;
-  name: string;
-  localForm: UseFormReturn;
+  label?: string; // optional, displays label if provided
+  name?: string; // optional, not required since no form control
+  tip?: string; // optional, displays tip if provided
+  defaultValue?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
 };
 
 type ControlledTextareaProps = ChakraTextareaProps &
@@ -19,17 +16,22 @@ type ControlledTextareaProps = ChakraTextareaProps &
 const ControlledTextarea: React.FC<ControlledTextareaProps> = ({
   label,
   name,
-  localForm,
+  defaultValue,
+  onChange,
+  value,
   ...props
 }: ControlledTextareaProps) => {
-  const { register } = localForm;
-
   return (
-    <FormControl>
+    <>
       {label && <FormLabel>{label}</FormLabel>}
-      {/* @ts-ignore next-line */}
-      <ChakraTextarea {...props} {...register(name)} />
-    </FormControl>
+      <ChakraTextarea
+        name={name}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        value={value}
+        {...props} // catches other ChakraInput props such as placeholder
+      />
+    </>
   );
 };
 
