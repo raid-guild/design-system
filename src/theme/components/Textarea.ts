0e4dee0ac730363ec3b37e174526a9834743c76f
jonathanprozzi <jonathanprozzi@gmail.com>
2022-12-29 14:13:47 -0500

    textarea bg fixed

--- src/theme/components/Textarea.ts
+++ src/theme/components/Textarea.ts
@@ -3,6 +3,13 @@ const Textarea = {
   variants: {
     filled: {
       borderRadius: '0px',
+      bg: ['gray.700', 'gray.700'],
+      _focus: {
+        bg: ['gray.700', 'gray.700'],
+      },
+      _hover: {
+        bg: ['gray.700', 'gray.700'],
+      },
     },
     outline: {
       border: '1px solid',
