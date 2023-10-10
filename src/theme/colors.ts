d2c899260d42a094955515e2b0d628fed2fded94
memosys <82053242+memosys@users.noreply.github.com>
2023-10-10 12:52:28 +0530

    wip
    check changes for more info

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,30 +1,22 @@
+import _ from 'lodash';
+import { RGTokens } from 'components/tokens';
+
+interface colorTokenInterface {
+  value: string;
+  type: string;
+}
+
+const chakraColors = _.mapValues(RGTokens.colors, (colors) =>
+  _.mapValues(colors, (color: colorTokenInterface) => color.value)
+);
+
 const Colors = {
-  white: '#fffffe',
-  purple: {
-    500: '#822EA6',
-  },
-  red: {
-    500: '#ff3864',
-  },
-  yellow: {
-    500: '#F2E857',
-  },
+  ...chakraColors,
   headerGradient: {
     500: 'linear-gradient(96.18deg, rgb(255, 56, 100) -44.29%, rgb(139, 29, 186) 53.18%, rgb(67, 83, 223) 150.65%)',
   },
   rgGradient: {
     500: 'linear-gradient(95.58deg, #FF3864 0%, #8B1DBA 53.65%, #4353DF 100%)',
-  },
-  primary: {
-    // Base colour is at 500
-    50: '#ff95ad',
-    200: '#ff95ad',
-    300: '#ff6788',
-    400: '#ff577c',
-    500: '#ff3864',
-    600: '#ff194c',
-    700: '#ff0940',
-    800: '#da0030',
   },
 };
 
